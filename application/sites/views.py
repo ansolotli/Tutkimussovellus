cc273e14b0a1707660509b66dc6e2f7377886ef6
ansolotli <annaliinakoskinen@gmail.com>
2019-02-21 00:48:42 +0200

    pagination for site list

--- application/sites/views.py
+++ application/sites/views.py
@@ -11,7 +11,20 @@ from application.auth.models import User
 
 @app.route("/sites", methods=["GET"])
 def sites_index():
-    return render_template("sites/list.html", sites = Site.query.all())
+    page = request.args.get('page', 1, type=int)
+    per_page = 10
+    sites = Site.query.order_by(Site.name).paginate(page, per_page, error_out = False)
+    
+# miten deaktivoida previous- ja next-linkit, jos näytettäviä kohteita on None?
+
+    next_url = url_for('sites_index', page=sites.next_num) \
+        if sites.has_next else None
+    prev_url = url_for('sites_index', page=sites.prev_num) \
+        if sites.has_prev else None
+
+    return render_template("sites/list.html", sites = sites.items,
+                          next_url=next_url, prev_url=prev_url)
+
     
 
 @app.route("/sites/<site_id>", methods=["GET"])
@@ -78,7 +91,6 @@ def sites_rename(site_id):
         return render_template("sites/single.html", site=s, form=form)
 
     s.name = form.name.data
-    
     db.session.commit()
 
     return redirect(url_for("sites_show_one_site", site_id=site_id)) 
