37b2471b8f4bb541113dd66ed41a11a94b8473b8
ansolotli <annaliinakoskinen@gmail.com>
2019-02-21 16:57:00 +0200

    Adding the possibility to edit samples

--- application/samples/forms.py
+++ application/samples/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators
 
-class SampleForm(FlaskForm):
+class AddSampleForm(FlaskForm):
     site_id = StringField("Site")
     samplename = StringField("Sample name", [validators.Length(min=2)])
     sampletype = StringField("Sample type", [validators.Length(min=2)])
@@ -11,7 +11,17 @@ class SampleForm(FlaskForm):
     class Meta:
         csrf = False
 
-class UpdateSampleForm(FlaskForm):
+class ShowSampleForm(FlaskForm):
+    samplename = StringField("Sample name", [validators.Length(min=2)])
+    sampletype = StringField("Sample type", [validators.Length(min=2)])
+    species = StringField("Species", [validators.Length(min=2)])
+    amount = StringField("Number of Specimen")
+    
+    class Meta:
+        csrf = False
+
+class EditSampleForm(FlaskForm):
+    sitename = StringField("Site")
     samplename = StringField("Sample name", [validators.Length(min=2)])
     sampletype = StringField("Sample type", [validators.Length(min=2)])
     species = StringField("Species", [validators.Length(min=2)])
--- application/samples/views.py
+++ application/samples/views.py
@@ -4,7 +4,7 @@ from sqlalchemy import text
 
 from application import app, db, login_required
 from application.samples.models import Sample
-from application.samples.forms import SampleForm, UpdateSampleForm
+from application.samples.forms import AddSampleForm, ShowSampleForm, EditSampleForm
 from application.sites.models import Site
 from application.auth.models import User
 
@@ -16,7 +16,7 @@ def samples_index():
 
 @app.route("/samples/new/", methods=["GET"])
 def samples_form():
-    return render_template("samples/new.html", form = SampleForm(), sites = Site.query.all())
+    return render_template("samples/new.html", form = AddSampleForm(), sites = Site.query.all())
 
 
 @app.route("/sites/samples/<sample_id>")
@@ -25,13 +25,13 @@ def samples_show_one_sample(sample_id):
     sample = Sample.query.get(sample_id)
     site = Site.query.get(sample.site_id)
 
-    return render_template("samples/single.html", form = UpdateSampleForm, sample=sample, site = site)
+    return render_template("samples/single.html", form = ShowSampleForm, sample=sample, site = site)
 
 
 @app.route("/samples/add", methods=["POST"])
 @login_required()
 def samples_create():
-    form = SampleForm(request.form)
+    form = AddSampleForm(request.form)
 
     if not form.validate():
         return render_template("samples/new.html", form = form)
@@ -44,9 +44,6 @@ def samples_create():
     s.site_id = site.id
 
     db.session().add(s)
-    
-    # lisätään sample tietylle saitille?
-    # site.samples.append(s)
     user.mysamples.append(s)
     db.session().commit()
 
@@ -63,3 +60,38 @@ def samples_remove(sample_id):
     db.session().commit()
 
     return redirect(url_for("sites_index"))
+
+
+@app.route("/samples/update/<sample_id>", methods=["GET", "POST"])
+@login_required()
+def samples_update(sample_id):
+
+    sample = Sample.query.get(sample_id)
+
+    return render_template("samples/edit.html", sample = sample, form=EditSampleForm(), sites = Site.query.all())
+
+
+@app.route("/samples/save/<sample_id>", methods=["GET", "POST"])
+@login_required()
+def samples_save(sample_id):
+    form = EditSampleForm(request.form)
+
+    s = Sample.query.filter_by(id=sample_id).first()
+
+    if not form.validate():
+        return render_template("samples/show_one_sample.html", sample_id=sample_id, form = form)
+
+    # jos näitä ei muuteta niin pidä vanhat!!
+
+    siteName = request.form.get("sites")
+    site = Site.query.filter_by(name=siteName).first()
+    s.site_id = site.id
+
+    s.samplename = form.samplename.data
+    s.sampletype = form.sampletype.data
+    s.species = form.species.data
+    s.amount = form.amount.data
+
+    db.session.commit()
+
+    return redirect(url_for("samples_show_one_sample", sample_id=sample_id)) 
